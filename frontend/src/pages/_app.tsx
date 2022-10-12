d631c27d5d994e9065af65256f152ae73273c9b8
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-10-12 13:35:31 +0300

    nextauth logic added, prisma, mongodb

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import { Session } from "next-auth/core/types";
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<SessionProviderProps>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <div>hello</div>;
+  const { data: session, status } = useSession();
+  console.log("session", session, status);
+  return (
+    <div>
+      {session?.user ? (
+        <button onClick={() => signOut()}>sign out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>sign in</button>
+      )}
+      <p>{session?.user?.name}</p>
+    </div>
+  );
 };
 
 export default Home;
