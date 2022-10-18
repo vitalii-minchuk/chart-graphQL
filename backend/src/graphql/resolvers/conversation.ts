bbbc274cf895488032a6642a01a4150aba519073
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-10-18 21:33:54 +0300

    create conversation

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,12 +1,76 @@
+import { Prisma } from "@prisma/client";
+import { ApolloError } from "apollo-server-core";
 import { GraphQLContext } from "./../../util/types";
+
 const resolvers = {
   Mutation: {
     createConversation: async (
       _: any,
       args: { participantIds: string[] },
       context: GraphQLContext
-    ) => {},
+    ): Promise<{ conversationId: string }> => {
+      const { participantIds } = args;
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        throw new ApolloError("Not authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          include: conversationPopulated,
+        });
+
+        //emit CONVERSATION_CREATED event using pubsub
+
+        return { conversationId: conversation.id };
+      } catch (error) {
+        console.log("createConversation error", error);
+        throw new ApolloError("Error creating conversation");
+      }
+    },
   },
 };
+
+export const participantPopulated =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulated,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
 
 export default resolvers;
